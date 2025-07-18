cc60ed5c5875ab3eebedc2371d228250e663479b
faithakinbobola <faithakinbobola2@gmail.com>
2025-07-18 17:03:26 +0100

    Advanced Task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,10 +7,10 @@ const Landing: React.FC =  () => {
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
       <div className="flex flex-wrap gap-4">
-        <Button title="Small Rounded" styles="px-4 py-1 text-sm bg-blue-500 text-white rounded-sm" />
-        <Button title="Medium Rounded" styles="px-5 py-2 text-base bg-green-500 text-white rounded-md" />
-        <Button title="Large Rounded" styles="px-6 py-3 text-lg bg-red-500 text-white rounded-full" />
-        <Button title="Extra Rounded (LG)" styles="px-5 py-2 text-base bg-purple-500 text-white rounded-lg" />
+        <Button title="Small" styles="px-4 py-1 text-sm bg-blue-500 text-white rounded-sm" />
+        <Button title="Medium" styles="px-5 py-2 text-base bg-green-500 text-white rounded-md" />
+        <Button title="Large" styles="px-6 py-3 text-lg bg-red-500 text-white rounded-full" />
+        <Button title="Extra" styles="px-5 py-2 text-base bg-purple-500 text-white rounded-lg" />
       </div>
     </div>
   )
